1a1d8a39427b5f004bd1357ce94d0786562579d3
Floyd Terbo <fterbo@protonmail.com>
2018-05-04 08:16:06 -0400

    Rename tools, add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2018  Floyd Terbo
+
+from distutils.core import setup
+import setuptools
+
+
+setup(name = "scotus-tools",
+      version = "0.9.0",
+      author = "Floyd Terbo",
+      author_email = "fterbo@protonmail.com",
+      packages = setuptools.find_packages(),
+      install_requires = [
+        "BeautifulSoup",
+        "PyPDF2",
+        "requests",
+      ],
+      zip_safe = False,
+      scripts = ['tools/ordergrab', 'tools/docketgrab', 'tools/orderparse']
+  )
