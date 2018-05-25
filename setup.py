9d1bb3ecd0acffbf5b40473a6f7b44f2355c94fd
Floyd Terbo <fterbo@protonmail.com>
2018-05-24 20:47:45 -0500

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 
 setup(name = "scotus-tools",
-      version = "0.9.6",
+      version = "0.9.7",
       author = "Floyd Terbo",
       author_email = "fterbo@protonmail.com",
       packages = setuptools.find_packages(),
