a31a551b980889acbfe44f2b82024f08bd8d5b82
Floyd Terbo <fterbo@protonmail.com>
2018-07-12 08:26:25 -0400

    Reorder attorneys alphabetically by last name, add a few, some warning comments

--- scotus/attorneys.py
+++ scotus/attorneys.py
@@ -73,34 +73,48 @@ class Attorney(object):
     return False
 
   def isPrivate (self, qdate):
+    # Gov't practice is much easier to track than moves in private firms, so we just assume they were in
+    # private practice for dates not covered by known government service.  Of course if you supply some
+    # random date instead of one retrieved from a filing, we'll be happy to report private practive even
+    # if they weren't alive.
     return not self.isGov(qdate)
 
 
-# Many of these are conservative date ranges based on filings, they are definitely NOT accurate
+# Many of these are conservative date ranges based on filings, they are definitely NOT complete or accurate 
 
 _attys = [
+  Attorney("Paul D. Clement")
+    .setFirm("kirkland-ellis", date(2016, 9, 12), None)
+    .setFirm("bancroft", date(2011, 3, 25), date(2016, 9, 12))
+    .setFirm("king-spalding", date(2008, 11, 20), date(2011, 3, 25))
+    .setSG("us.deputy", date(2001, 2, 1), date(2004, 7, 11))
+    .setSG("us.acting", date(2004, 7, 11), date(2005, 6, 13))
+    .setSG("us", date(2005, 6, 13), date(2008, 6, 19)),
+  Attorney("Trevor Stephen Cox")
+    .setAG("va.unknown", date(2017, 2, 21), None),
+  Attorney("Stephen R. Creason", "Stephen Richard Creason")
+    .setAG("in.unknown", date(2006, 12, 13), None),
   Attorney("Noel J. Francisco", "Noel Francisco")
     .setSG("us.acting", date(2017, 1, 23), date(2017, 3, 10))
     .setSG("us", date(2017, 9, 19), None),
+  Attorney("Michael Marc Glick")
+    .setAG("illinois.ccad", date(2006, 7, 1), None),
+  Attorney("John M. Klawikofsky")
+    .setAG("fl.unknown", date(2005, 3, 14), None),
   Attorney("Aaron David Lindstrom", "Aaron D. Lindstrom Jr.")
     .setSG("mi", date(2013, 12, 1), None)
     .setSG("mi.assistant", date(2012, 10, 1), date(2013, 11, 30))
     .setFirm("warner-norcross-judd", date(2009, 5, 1), date(2012, 9, 30))
     .setFirm("gibson-dunn-crutcher", date(2005, 10, 1), date(2009, 4, 30)),
-  Attorney("Michael Marc Glick")
-    .setAG("illinois.ccad", date(2006, 7, 1), None),
-  Attorney("Kevin Joel Page")
-    .setFPD("tx", date(2016, 5, 1), None),
+  Attorney("Matthew Robert McGuire")
+    .setAG("va.unknown", date(2017, 2, 21), None),
   Attorney("Eric E. Murphy")
     .setSG("oh", date(2013, 9, 9), None),
-  Attorney("Matthem Robert McGuire")
-    .setAG("va.unknown", date(2017, 2, 21), None),
-  Attorney("Trevor Stephen Cox")
-    .setAG("va.unknown", date(2017, 2, 21), None),
-  Attorney("John M. Klawikofsky")
-    .setAG("fl.unknown", date(2005, 3, 14), None),
-  Attorney("Stephen R. Creason", "Stephen Richard Creason")
-    .setAG("in.unknown", date(2006, 12, 13), None),
+  Attorney("Kevin Joel Page")
+    .setFPD("tx", date(2016, 5, 1), None),
+  Attorney("Kannon K. Shanmugam")
+    .setSG("us.assistant", date(2003, 6, 23), date(2008, 3, 24))
+    .setFirm("williams-connolly", date(2008, 5, 2), None)
 ]
 
 
