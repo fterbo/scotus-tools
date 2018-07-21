f0c47e772d67a3882c29c7d509f5d95cd05c41a2
Floyd Terbo <fterbo@protonmail.com>
2018-07-20 22:48:41 -0400

    Allow scan to have a range

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 
 setup(name = "scotus-tools",
-      version = "0.9.32",
+      version = "0.9.33",
       author = "Floyd Terbo",
       author_email = "fterbo@protonmail.com",
       packages = setuptools.find_packages(),
