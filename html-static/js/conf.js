24377caf1e61787a362a5162f1234750ad5451c9
Adam Blomeke <adam.blomeke@itworksdc.com>
2019-01-24 22:08:41 -0500

    added several columns, corrected others

--- html-static/js/conf.js
+++ html-static/js/conf.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  $.getJSON("data/conf/20190215.json", function(data){
+  $.getJSON("data/conf/20190111.json", function(data){
     GenerateTable(data);
   });
   var caseName = "case-name";
@@ -13,41 +13,88 @@
   var distDetails = "dist-details";
   var reschCount = "resch-count";
   var caseType = "case-type";
+  var currentStatus = "current-status";
+  var flags = "flags"
   function GenerateTable(data) {
     for(var i=0; i<data.dockets.length; i++){
       var thisCase = data.dockets[i]
       $("#conf > tbody:last-child").append
       (
-        "<tr id=\""+thisCase[docketStr]+"\">"+
-          TdTag(docketStr, thisCase[docketStr]) +
+        "<tr id=\""+thisCase[docketStr]+"\" "+addClass(thisCase[currentStatus])+">"+
+          TdTag(docketStr, docketNumberLink(thisCase)) +
+          TdTag(caseType, thisCase[caseType]) +
           TdTag(lcAbbr,thisCase[lcAbbr]) +
           TdTag(
             caseName+"-td",
-            details(
-              thisCase[caseName],
-              caseName+"-summary",
-              thisCase[qp],
-              qp
-            )
+            caseNameTD(thisCase)
           ) +
+          TdTag(flags, flagsColumn(thisCase[flags]))+
           TdTag(
             "dist-td",
             details(
-              thisCase[distCount] + "("+thisCase[reschCount] + " Resch)",
+              thisCase[distCount] + "(" + thisCase[reschCount] + " Resch)",
               "dist-summary",
               thisCase[distDetails],
               distDetails)
             ) +
+            TdTag(currentStatus, thisCase[currentStatus])+
         "</tr>"
       );
     }
   }
-  function TdTag(id, content){
-    return "<td id=\""+ id + "\">" + content + "</td>";
+  function TdTag(id, content, classes){
+    return "<td id=\""+ id + "\""
+    + addClass(classes)+" >" + content + "</td>";
+  }
+  function addClass(classes) {
+    if (typeof classes === 'undefined' || classes === null ){
+      return "";
+    } else {
+      return "class=\""+ classes + "\""
+    }
   }
   function details(summary, summaryID, pre, preID) {
       return "<details> <summary id =\""+summaryID+"\">"
       + summary + "</summary> "+
       "<pre id=\""+ preID + "\">" + pre + "</pre> </details>";
+  }
+  function translateTags(thisCaseFlags) {
+    var result = "";
+    if(thisCaseFlags.capital){
+      result += "{CAPITAL}";
+    }
+    if(thisCaseFlags.related){
+      result += "{RELATED}";
+    }
+    if(thisCaseFlags.cvsg){
+      result += "{CVSG}"
+    }
+    return result.replace("}{", "}</br>{" )
+  }
+  function flagsExist(thisCaseFlags) {
+    return thisCaseFlags.capital || thisCaseFlags.related || thisCaseFlags.cvsg;
+  }
+  function flagsColumn(thisCaseFlags){
+    if(flagsExist(thisCaseFlags)){
+      return translateTags(thisCaseFlags);
+    } else {
+      return "";
+    }
+  }
+  function docketNumberLink(thisCase) {
+    return "<a href=\""+thisCase[docketUrl] + "\">"+ thisCase[docketStr] + "</a>";
+  }
+  function caseNameTD(thisCase){
+    var caseQp = thisCase[qp];
+    if (typeof caseQp === 'undefined' || caseQp === null ){
+      return thisCase[caseName] ;
+    } else {
+      return details(
+        thisCase[caseName],
+        caseName+"-summary",
+        thisCase[qp],
+        qp
+      );
+    }
   }
 }());
