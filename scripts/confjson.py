ed48696983be14f3593e6ae0a2b7b13ca64f339d
Floyd Terbo <fterbo@protonmail.com>
2019-05-08 16:48:17 -0400

    Log exceptions with docket

--- scripts/confjson.py
+++ scripts/confjson.py
@@ -42,65 +42,68 @@ def main():
     djson = json.loads(open("OT-%d/dockets/%d/docket.json" % (term, num), "rb").read())
     docket = scotus.util.DocketStatusInfo(djson)
 
-    lcinfo = []
-    lcinfo.append(docket.lowercourt)
-    if docket.lowercourt_decision_date:
-      lcinfo.append("%s - %s" % (docket.lowercourt_docket, docket.lowercourt_decision_date.strftime("%Y-%m-%d")))
-    else:
-      lcinfo.append(docket.lowercourt_docket)
-
-    rd = {}
-
-    dstrl = []
-    dcount = 0
-    rdcount = 0
-    ddate = None
-    for (ed, cd, r) in docket.distributed:
-      if cd == cdate:
-        ddate = ed
-        rd["dist-date"] = {"y" : ed.year, "m" : ed.month, "d" : ed.day}
-      dcount += 1
-      cds = cd.strftime("%Y-%m-%d")
-      if r:
-        dstrl.append("%s[R]" % (cds))
-        rdcount += 1
+    try:
+      lcinfo = []
+      lcinfo.append(docket.lowercourt)
+      if docket.lowercourt_decision_date:
+        lcinfo.append("%s - %s" % (docket.lowercourt_docket, docket.lowercourt_decision_date.strftime("%Y-%m-%d")))
       else:
-        dstrl.append(cds)
+        lcinfo.append(docket.lowercourt_docket)
 
-    skip = False
-    for event in docket.events:
-      if event.date >= ddate and event.date <= cdate:
-        if event.response_requested or event.record_requested:
-          skip = True
-          break
+      rd = {}
 
-    if skip:
-      logging.debug("%s: skipping %s" % (cshortstr, docket.docketstr))
-      continue
+      dstrl = []
+      dcount = 0
+      rdcount = 0
+      ddate = None
+      for (ed, cd, r) in docket.distributed:
+        if cd == cdate:
+          ddate = ed
+          rd["dist-date"] = {"y" : ed.year, "m" : ed.month, "d" : ed.day}
+        dcount += 1
+        cds = cd.strftime("%Y-%m-%d")
+        if r:
+          dstrl.append("%s[R]" % (cds))
+          rdcount += 1
+        else:
+          dstrl.append(cds)
+
+      skip = False
+      for event in docket.events:
+        if event.date >= ddate and event.date <= cdate:
+          if event.response_requested or event.record_requested:
+            skip = True
+            break
+
+      if skip:
+        logging.debug("%s: skipping %s" % (cshortstr, docket.docketstr))
+        continue
 
 
-    rd["docket-url"] = docket.docketurl
-    rd["docket-str"] = docket.docketstr
-    rd["case-name"] = docket.casename
-    rd["case-type"] = docket.casetype
-    rd["current-status"] = docket.current_status
-    rd["conf-action"] = docket.getConfAction(cdate, dates)
-    rd["dist-count"] = dcount
-    rd["resch-count"] = rdcount
-    rd["dist-details"] = "<br>".join(dstrl)
-    rd["flags"] = docket.getFlagDict()
-    rd["tags"] = docket.getTagDict()
-    rd["holding"] = docket.getHoldingDict()
+      rd["docket-url"] = docket.docketurl
+      rd["docket-str"] = docket.docketstr
+      rd["case-name"] = docket.casename
+      rd["case-type"] = docket.casetype
+      rd["current-status"] = docket.current_status
+      rd["conf-action"] = docket.getConfAction(cdate, dates)
+      rd["dist-count"] = dcount
+      rd["resch-count"] = rdcount
+      rd["dist-details"] = "<br>".join(dstrl)
+      rd["flags"] = docket.getFlagDict()
+      rd["tags"] = docket.getTagDict()
+      rd["holding"] = docket.getHoldingDict()
 
-    rd["lc-abbr"] = cabbr
-    if cabbr != "None":
-      rd["lc-info"] = "<br>".join([str(x) for x in lcinfo])
+      rd["lc-abbr"] = cabbr
+      if cabbr != "None":
+        rd["lc-info"] = "<br>".join([str(x) for x in lcinfo])
 
-    qp = docket.getQPText().strip().decode("utf-8")
-    if qp:
-      rd["qp"] = qp
+      qp = docket.getQPText().strip().decode("utf-8")
+      if qp:
+        rd["qp"] = qp
 
-    tdata.append(rd)
+      tdata.append(rd)
+    except Exception as e:
+      logging.exception("Docket %d-%d" % (term, num))
 
   outdict["dockets"] = tdata
 
