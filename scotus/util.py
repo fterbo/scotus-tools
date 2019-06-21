bbe9765506a18fb920cbdd41d0a944eae42d577d
Floyd Terbo <fterbo@protonmail.com>
2019-06-21 18:43:25 -0400

    Some times we don't use periods.  Some times we do.

--- scotus/util.py
+++ scotus/util.py
@@ -358,6 +358,8 @@ class DocketStatusInfo(object):
         estxt = etxt
         if etxt[-6:] == "VIDED.":
           estxt = etxt[:-6]
+        elif etxt[-5:] == "VIDED":
+          estxt = etxt[:-5]
 
         if etxt.startswith("DISTRIBUTED"):
           if etxt == "DISTRIBUTED.":
